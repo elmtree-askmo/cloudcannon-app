e77e9fdc4ae2e634f2457a4575118310719f67e9
Allen <79867794@qq.com>
2023-10-23 14:44:50 +0800

    update home page

--- app/component/footer.js
+++ app/component/footer.js
@@ -1,10 +1,10 @@
 import Link from "next/link";
-import './footer.css';
+import styles from './footer.module.css';
 
 
 export default function Footer(){
     return (
-        <div className="footer">
+        <div className={styles["footer"]}>
             <p>
                 EdKey, Inc. All Rights Reserved © 2023 | <Link href="https://app.quicktakes.io/terms" target="_blank">Terms of Service</Link> | <Link href="https://app.quicktakes.io/privacy-policy" target="_blank" >Privacy Policy</Link> | Contact
             </p>
--- app/component/header.js
+++ app/component/header.js
@@ -2,14 +2,16 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import "./header.css";
+import styles from "./header.module.css";
 import { Button, Drawer, Input, Modal } from "antd";
 import { useState } from "react";
 import { APP_URL } from "../global/global";
+import { useRouter } from "next/navigation";
 
 export default function Header(){
     const [openMenu, setOpenMenu] = useState(false);
     const [openContact, setOpenContact] = useState(false);
+    const router = useRouter();
     const handleSubmit =(e)=>{
         e.preventDefault();
         const form = e.target;
@@ -25,11 +27,14 @@ export default function Header(){
     const handleLogin = ()=>{
         window.location.href = `https://${APP_URL}`
     }
+    const handleClickLogo = ()=>{
+        router.push('/')
+    }
 
     return (
-        <div className="header">
-            <div className="main-container desktop-view">
-                <div className="logo-container">
+        <div className={styles.header}>
+            <div className={styles['main-container']}>
+                <div className={styles['logo-container']} onClick={handleClickLogo}>
                     <Image 
                         src="/quicktakesIcon.svg"
                         alt="Logo"
@@ -39,16 +44,16 @@ export default function Header(){
                     />
                     <p><strong>QuickTakes</strong> for Students</p>
                 </div>
-                <div className="menu-container">
+                <div className={styles["menu-container"]}>
                     <span onClick={()=>{setOpenContact(true)}}>Contact</span>
                     <Link href="/about-us">About us</Link>
                     <Link href="/faq">FAQ</Link>
-                    <i className="sign-up-button" onClick={handleSignUp} >Sign Up</i>
+                    <i className={styles["sign-up-button"]} onClick={handleSignUp} >Sign Up</i>
                     <Link href="" onClick={handleLogin}>Log In</Link>
                     <Image 
                         src="/menu.svg"
                         alt="menu"
-                        className="menu"
+                        className={styles["menu"]}
                         width={28}
                         height={28}
                         onClick={()=>{setOpenMenu(true)}}
@@ -65,20 +70,20 @@ export default function Header(){
                 onClose={()=>{setOpenMenu(false)}}
                 closeIcon={false}
                 mask={false}
-                classNames={{body:"custom-drawer-body"}}
+                classNames={{body:styles["custom-drawer-body"]}}
                 destroyOnClose={true}
             >
-                <div className="drawer-header">
+                <div className={styles["drawer-header"]}>
                     <Image 
                         src="/close.svg"
                         alt="close"
-                        className="close"
+                        className={styles["close"]}
                         width={28}
                         height={28}
                         onClick={()=>{setOpenMenu(false)}}
                     />
                 </div>
-                <div className="drawer-container">
+                <div className={styles["drawer-container"]}>
                     <Link href="/about-us">About us</Link>
                     <Link href="">Log In</Link>
                     <Link href="/faq">FAQ</Link>
@@ -92,17 +97,17 @@ export default function Header(){
                 centered
                 footer={null}
                 onCancel={()=>{setOpenContact(false)}}
-                className="custom-modal"
+                className={styles["custom-modal"]}
                 width={"auto"}
                 maskClosable={false}
             >
-                <div className="contact-form-container">
+                <div className={styles["contact-form-container"]}>
                     <form onSubmit={handleSubmit} >
                         <div>Contanct us:</div>
                         <Input placeholder="Name" name="name" />
                         <Input placeholder="Email" name="email" />
                         <Input placeholder="Description" name="description" />
-                        <Button type="primary" className="send" htmlType="submit" >Send</Button>
+                        <Button type="primary" className={styles["send"]} htmlType="submit" >Send</Button>
                     </form>
                 </div>
             </Modal>
