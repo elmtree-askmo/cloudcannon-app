eb4617279c0ed451c359411168238ba4bc9650e7
Sunny <sunnyzhang@cherrypicks.com>
2023-10-24 15:21:02 +0800

    handle inner-link hint text

--- app/component/sign-up-now.js
+++ app/component/sign-up-now.js
@@ -30,6 +30,7 @@ export default function SignUpNow() {
           <p>Find Success.</p>
         </div>
         <img className={styles['inner-image']} src="./images/sign-up-now-image.webp" />
+        <div className={styles['inner-hint']}><span>FREE</span> for students, forever!</div>
         <div className={styles['sign-up-button']}><Button onClick={SignUpNow}>Sign Up Now</Button></div>
         <div className={styles["download-title"]}>Download <span>QuickTakes</span> today!</div>
         <div className={styles['download-links']}>
