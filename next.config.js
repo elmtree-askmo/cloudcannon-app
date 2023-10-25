64a9951a0f7c442107493f5192be50660a10a9cb
Allen <79867794@qq.com>
2023-10-25 14:32:01 +0800

    rollback

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output:'export',
-    exportPathMap:async function () {
-        return {
-            '/':{ page: '/'},
-            '/faq':{ page: '/faq'}
-        }
-    }
 }
 
 module.exports = nextConfig
