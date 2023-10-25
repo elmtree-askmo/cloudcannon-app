96c2a57c42cc9f36d8b1da996a56a6d85d224c2b
Allen Chen <allenchen@cherrypicks.com>
2023-10-25 07:57:02 +0000

    Updated 1 file via CloudCannon.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
-const { generateStaticParams } = require('next/dist/build/generate-static-params');
 const nextConfig = {
     output:'export',
-    async generateStaticParams() {
-        return await generateStaticParams({
-          '/': { page: '/' },
-          '/faq.html': { page: '/faq.html' },
-        });
-    },
 }
 
 module.exports = nextConfig
