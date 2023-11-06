1e4887a0e634d74122a1b5ee523696b547099d1b
Allen <79867794@qq.com>
2023-11-06 17:34:42 +0800

    test smart app banner

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <link rel="stylesheet" href="node_modules/smartbanner.js/dist/smartbanner.min.css" /> 
+          <Script src="node_modules/smartbanner.js/dist/smartbanner.min.js"></Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
