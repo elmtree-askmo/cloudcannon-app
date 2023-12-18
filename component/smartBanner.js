910d6d2e4b5f4029e8ae1b577078f814f629a423
Allen <79867794@qq.com>
2023-12-18 11:29:29 +0800

    try to use new deeplink to solve issue

--- component/smartBanner.js
+++ component/smartBanner.js
@@ -6,8 +6,8 @@ import mixpanel from "mixpanel-browser";
 
 export default function SmartBanner({hideBanner, role, pageStr}){
 
-    const deepLink = 'https://link.quicktakes.io/api-node/quicktake/dl?url=quicktakes%3A%2F%2Fmain%2Fquicktakes?action%3Dsign-up%26utm_medium%3Dmkt_site%26utm_source%3Dbtn_view'
-    
+    // const deepLink = 'https://link.quicktakes.io/api-node/quicktake/dl?url=quicktakes%3A%2F%2Fmain%2Fquicktakes?action%3Dsign-up%26utm_medium%3Dmkt_site%26utm_source%3Dbtn_view'
+    const deepLink = 'https://yjsnh.app.link/verification-sign-up'
     const closeBanner = ()=>{
         localStorage.setItem('hideBannerTS',new Date().valueOf());
         hideBanner();
