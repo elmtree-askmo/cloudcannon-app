8d8012290a4b9b273839f07c5965db7b08a07ee5
Allen <79867794@qq.com>
2024-04-08 17:48:19 +0800

    update school section

--- components/home/schools.js
+++ components/home/schools.js
@@ -1,17 +1,43 @@
+import { useRef } from 'react';
 import styles from '../../styles/Home.module.css';
+import { Carousel } from 'antd';
+import SlideArrowLeft from '@/public/slide-arrow-left';
+import SlideArrowRight from '@/public/slide-arrow-right';
 
 export default function Schools({content, block}){
+    const carouselRef = useRef();
+    
+    const onPrev = ()=>{
+        carouselRef.current.prev && carouselRef.current.prev();
+    }
+    const onNext = ()=>{
+        carouselRef.current.next && carouselRef.current.next();
+    }
     return (
         <div className={styles['schools-container']}>
             <div className={styles['schools-center-container']} >
                 <h3>{block.contentTitle}</h3>
-                {
-                    block.content.map((item, index)=>(
-                        <div key={index} className={item.reverse? styles['scrolling-reverse']:styles.scrolling}>
-                            <p>{item.line}</p>
-                        </div>
-                    ))
-                }
+                <div className={styles['schools-carousel-container']}>
+                    <Carousel dots={false}  ref={carouselRef} autoplaySpeed={5000} waitForAnimate autoplay > 
+                        {
+                            block.content.map((item, index)=>(
+                                <div className={styles['schools-carousel-item']} key={index}>
+                                    {
+                                        item.slide.map((subItem, i)=>(
+                                            <div key={i}><img src={subItem.schoolLogo} /></div>
+                                        ))
+                                    }
+                                </div>
+                            ))
+                        }
+                    </Carousel>
+                    <div className={styles.arrowLeft} onClick={onPrev}>
+                        <SlideArrowLeft className={styles['schools-arrowIcon']} />
+                    </div>
+                    <div className={styles.arrowRight} onClick={onNext}>
+                        <SlideArrowRight className={styles['schools-arrowIcon']} />
+                    </div>
+                </div>
             </div>
         </div>
     )
