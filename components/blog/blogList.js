79fc68bb2bf5e9c167eab76f4edc949eca0514be
Allen <allenchen@cherrypicks.com>
2024-05-23 11:27:51 +0800

    remove dummy data

--- components/blog/blogList.js
+++ components/blog/blogList.js
@@ -18,30 +18,6 @@ export default function BlogList({content, block, posts}){
                             </Link>
                         ))
                     }
-                    <div className={styles['blogList-list-item']}>
-                        <div><img src="https://www.studyfetch.com/_next/image?url=https%3A%2F%2Fstudyfetchcms.com%2Fcontent%2Fimages%2F2024%2F05%2Fangelawan9_cartoon_study_aesthetic_a34efa92-3cf8-4982-ac35-543aa3dba4dd.png&w=1920&q=75&dpl=dpl_TrkEvLzBXGAxH4eiunmdvuuyqmDt" /></div>
-                        <h3>5 Study Methods to Live By</h3>
-                        <p>Discover unconventional study methods for college success! From Memory Palaces to Sleep Studying, learn how to enhance learning efficiency and conquer academic challenges. Embrace new strategies to navigate the modern education landscape with confidence.</p>
-                        <span>Posted on 5/9/2024</span>
-                    </div>
-                    <div className={styles['blogList-list-item']}>
-                        <div><img src="https://www.studyfetch.com/_next/image?url=https%3A%2F%2Fstudyfetchcms.com%2Fcontent%2Fimages%2F2024%2F05%2Fangelawan9_cartoon_study_aesthetic_a34efa92-3cf8-4982-ac35-543aa3dba4dd.png&w=1920&q=75&dpl=dpl_TrkEvLzBXGAxH4eiunmdvuuyqmDt" /></div>
-                        <h3>5 Study Methods to Live By</h3>
-                        <p>Discover unconventional study methods for college success! From Memory Palaces to Sleep Studying, learn how to enhance learning efficiency and conquer academic challenges. Embrace new strategies to navigate the modern education landscape with confidence.</p>
-                        <span>Posted on 5/9/2024</span>
-                    </div>
-                    <div className={styles['blogList-list-item']}>
-                        <div><img src="https://www.studyfetch.com/_next/image?url=https%3A%2F%2Fstudyfetchcms.com%2Fcontent%2Fimages%2F2024%2F05%2Fangelawan9_cartoon_study_aesthetic_a34efa92-3cf8-4982-ac35-543aa3dba4dd.png&w=1920&q=75&dpl=dpl_TrkEvLzBXGAxH4eiunmdvuuyqmDt" /></div>
-                        <h3>5 Study Methods to Live By</h3>
-                        <p>Discover unconventional study methods for college success! From Memory Palaces to Sleep Studying, learn how to enhance learning efficiency and conquer academic challenges. Embrace new strategies to navigate the modern education landscape with confidence.</p>
-                        <span>Posted on 5/9/2024</span>
-                    </div>
-                    <div className={styles['blogList-list-item']}>
-                        <div><img src="https://www.studyfetch.com/_next/image?url=https%3A%2F%2Fstudyfetchcms.com%2Fcontent%2Fimages%2F2024%2F05%2Fangelawan9_cartoon_study_aesthetic_a34efa92-3cf8-4982-ac35-543aa3dba4dd.png&w=1920&q=75&dpl=dpl_TrkEvLzBXGAxH4eiunmdvuuyqmDt" /></div>
-                        <h3>5 Study Methods to Live By</h3>
-                        <p>Discover unconventional study methods for college success! From Memory Palaces to Sleep Studying, learn how to enhance learning efficiency and conquer academic challenges. Embrace new strategies to navigate the modern education landscape with confidence.</p>
-                        <span>Posted on 5/9/2024</span>
-                    </div>
                 </div>
             </div>
         </div>
