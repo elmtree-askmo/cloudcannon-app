59de01b19f9e95e4dacf7f2b8d04c520fad998a1
Allen <allenchen@cherrypicks.com>
2024-07-25 11:12:40 +0800

    test mutiple language

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en" className='language-zh_cn'>
+    <Html lang="en" className='language-en'>
       <Head>
       </Head>
       <body>
