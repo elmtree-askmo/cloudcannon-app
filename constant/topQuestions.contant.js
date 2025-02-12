3a3417edeee9c9d95d8d2c0dc2a3bd6aca57838b
Sunny <sunnyzhang@cherrypicks.com>
2025-02-12 20:45:25 +0800

    updated learn files

--- constant/topQuestions.contant.js
+++ constant/topQuestions.contant.js
@@ -1,78 +1,78 @@
 export const TOP_QUESTIONS_SUBJECTS = [
-  {
-      "title": "Accounting and Finance",
-      "key": "accounting-and-finance"
-  },
-  {
-      "title": "Art History and Literature",
-      "key": "art-history-and-literature"
-  },
-  {
-      "title": "Biotechnology",
-      "key": "biotechnology"
-  },
-  {
-      "title": "Business and Management",
-      "key": "business-and-management"
-  },
-  {
-      "title": "Chemistry",
-      "key": "chemistry"
-  },
-  {
-      "title": "Computer Science",
-      "key": "computer-science"
-  },
-  {
-      "title": "Criminal or Civil Law",
-      "key": "criminal-or-civil-law"
-  },
-  {
-      "title": "Economics",
-      "key": "economics"
-  },
-  {
-      "title": "Education Studies",
-      "key": "education-studies"
-  },
-  {
-      "title": "Engineering",
-      "key": "engineering"
-  },
-  {
-      "title": "Health Studies",
-      "key": "health-studies"
-  },
-  {
-      "title": "History of the World",
-      "key": "history-of-the-world"
-  },
-  {
-      "title": "Mathematics",
-      "key": "mathematics"
-  },
-  {
-      "title": "Medicine",
-      "key": "medicine"
-  },
-  {
-      "title": "Nursing",
-      "key": "nursing"
-  },
-  {
-      "title": "Philosophy and Greek Myths",
-      "key": "philosophy-and-greek-myths"
-  },
-  {
-      "title": "Physics",
-      "key": "physics"
-  },
-  {
-      "title": "Psychology",
-      "key": "psychology"
-  },
-  {
-      "title": "Sociology",
-      "key": "sociology"
-  },
-  ]
+    {
+        "title": "Accounting and Finance",
+        "key": "accounting-and-finance"
+    },
+    {
+        "title": "Art History and Literature",
+        "key": "art-history-and-literature"
+    },
+    {
+        "title": "Biotechnology",
+        "key": "biotechnology"
+    },
+    {
+        "title": "Business and Management",
+        "key": "business-and-management"
+    },
+    {
+        "title": "Chemistry",
+        "key": "chemistry"
+    },
+    {
+        "title": "Computer Science",
+        "key": "computer-science"
+    },
+    {
+        "title": "Criminal or Civil Law",
+        "key": "criminal-or-civil-law"
+    },
+    {
+        "title": "Economics",
+        "key": "economics"
+    },
+    {
+        "title": "Education Studies",
+        "key": "education-studies"
+    },
+    {
+        "title": "Engineering",
+        "key": "engineering"
+    },
+    {
+        "title": "Health Studies",
+        "key": "health-studies"
+    },
+    {
+        "title": "History of the World",
+        "key": "history-of-the-world"
+    },
+    {
+        "title": "Mathematics",
+        "key": "mathematics"
+    },
+    {
+        "title": "Medicine",
+        "key": "medicine"
+    },
+    {
+        "title": "Nursing",
+        "key": "nursing"
+    },
+    {
+        "title": "Philosophy and Greek Myths",
+        "key": "philosophy-and-greek-myths"
+    },
+    {
+        "title": "Physics",
+        "key": "physics"
+    },
+    {
+        "title": "Psychology",
+        "key": "psychology"
+    },
+    {
+        "title": "Sociology",
+        "key": "sociology"
+    },
+]
