098af705b3d7ccdc4179a5b759cfb4893a85257c
Sunny <sunnyzhang@cherrypicks.com>
2025-03-11 14:26:41 +0800

    reset quicktakes icon

--- component/smartBanner.js
+++ component/smartBanner.js
@@ -1,32 +1,32 @@
-
-import { CloseOutlined } from '@ant-design/icons';
+import { CloseOutlined } from "@ant-design/icons";
 import mixpanel from "mixpanel-browser";
 
-import styles from './smartBanner.module.css';
+import styles from "./smartBanner.module.css";
 
 export default function SmartBanner({ hideBanner }) {
-
   // const deepLink = 'https://link.quicktakes.io/api-node/quicktake/dl?url=quicktakes%3A%2F%2Fmain%2Fquicktakes?action%3Dsign-up%26utm_medium%3Dmkt_site%26utm_source%3Dbtn_view'
-  const deepLink = 'https://link.quicktakes.io/KWZ8ZCWp1Fb';
+  const deepLink = "https://link.quicktakes.io/KWZ8ZCWp1Fb";
   const closeBanner = () => {
-    localStorage.setItem('hideBannerTS', new Date().valueOf());
+    localStorage.setItem("hideBannerTS", new Date().valueOf());
     hideBanner();
-  }
+  };
   const handleClickView = () => {
-    mixpanel.track(`MarketingPage_SmartBanner_View`)
-  }
+    mixpanel.track(`MarketingPage_SmartBanner_View`);
+  };
 
   return (
     <div className={styles.smart_banner_container}>
       <CloseOutlined className={styles.close} onClick={closeBanner} />
       <div className={styles.desc}>
-        <img src="../quicktakesIcon.svg" />
+        <img src="/quicktakesIcon.svg" />
         <div>
           <h3>QuickTakes by Edkey</h3>
           <p>Better College Class Notes!</p>
         </div>
       </div>
-      <a className={styles.cta} href={deepLink} target="_blank" onClick={handleClickView} >View</a>
+      <a className={styles.cta} href={deepLink} target="_blank" onClick={handleClickView}>
+        View
+      </a>
     </div>
-  )
+  );
 }
--- components/home/banner.js
+++ components/home/banner.js
@@ -1,53 +1,64 @@
-import mixpanel from 'mixpanel-browser';
-import { appStoreLink, googlePlayLink } from '@/constant/app.constant';
-import styles from '../../styles/Home.module.css';
-import { Button } from 'antd';
-import Link from 'next/link';
-import Image from 'next/image';
+import mixpanel from "mixpanel-browser";
+import { appStoreLink, googlePlayLink } from "@/constant/app.constant";
+import styles from "../../styles/Home.module.css";
+import { Button } from "antd";
+import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
 
-export default function Banner({content, block, language="en", utmParams=null}){
-    const handleSignUpNow = ()=>{
-      let deeplink = process.env.NEXT_PUBLIC_UTM_DEEPLINK;
-      if(utmParams){
-        const search = new URLSearchParams(utmParams).toString();
-        deeplink = `${deeplink}?${search}`;
-      }
-      mixpanel.track("MarketingPage_SignUp", { placement: 'homeBanner' }, {send_immediately:true}, ()=>{
-        window.location.href = deeplink;
-      })
+export default function Banner({ content, block, language = "en", utmParams = null }) {
+  const handleSignUpNow = () => {
+    let deeplink = process.env.NEXT_PUBLIC_UTM_DEEPLINK;
+    if (utmParams) {
+      const search = new URLSearchParams(utmParams).toString();
+      deeplink = `${deeplink}?${search}`;
     }
-    
-    return (
-        <div className={styles['section-1-container']}>
-          <div className={`${styles['section-1']}`}>
-            <div className={styles['section-1-center-container']}>
-              <div className={styles['section-1-major']}>
-                <h2>{block.contentTitle[language] || block.contentTitle['en']}</h2>
-                <h3>{block.subTitle[language] || block.subTitle['en']}</h3>
-                <Button type="primary" className={`custom-antd-design-button-student ${styles['sign-up-now']}`} onClick={handleSignUpNow}>{block.buttonTxt[language] || block.buttonTxt['en']}</Button>
-                <div className={styles['download-group']}>
-                  <Link href={appStoreLink} target='_blank'>
-                    <Image 
-                      src="/appStore.svg"
-                      alt="appStore"
-                      width={227}
-                      height={65}
-                      unoptimized
-                    />
-                  </Link>
-                  <Link href={googlePlayLink} target='_blank'>
-                    <Image 
-                      src="/googlePlay.svg"
-                      alt="googlePlay"
-                      width={227}
-                      height={65}
-                      unoptimized
-                    />
-                  </Link>
-                </div>
+    mixpanel.track("MarketingPage_SignUp", { placement: "homeBanner" }, { send_immediately: true }, () => {
+      window.location.href = deeplink;
+    });
+  };
+
+  return (
+    <>
+      <Head>
+        <link rel="preload" href="/appStore.svg" as="image" />
+        <link rel="preload" href="/googlePlay.svg" as="image" />
+      </Head>
+      <div className={styles["section-1-container"]}>
+        <div className={`${styles["section-1"]}`}>
+          <div className={styles["section-1-center-container"]}>
+            <div className={styles["section-1-major"]}>
+              <h2>{block.contentTitle[language] || block.contentTitle["en"]}</h2>
+              <h3>{block.subTitle[language] || block.subTitle["en"]}</h3>
+              <Button type="primary" className={`custom-antd-design-button-student ${styles["sign-up-now"]}`} onClick={handleSignUpNow}>
+                {block.buttonTxt[language] || block.buttonTxt["en"]}
+              </Button>
+              <div className={styles["download-group"]}>
+                <Link href={appStoreLink} target="_blank">
+                  <Image 
+                    src="/appStore.svg" 
+                    alt="appStore" 
+                    width={227} 
+                    height={65} 
+                    priority={true}
+                    loading="eager"
+                  />
+                </Link>
+                <Link href={googlePlayLink} target="_blank">
+                  <Image 
+                    src="/googlePlay.svg" 
+                    alt="googlePlay" 
+                    width={227} 
+                    height={65} 
+                    priority={true}
+                    loading="eager"
+                  />
+                </Link>
               </div>
             </div>
           </div>
         </div>
-    )
+      </div>
+    </>
+  );
 }
